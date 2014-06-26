71a29c4a3b3ddc54c6d215d73a6e69523c232084
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-06-26 17:05:59 +0200

    wip

---
+++ java-checks/src/test/files/checks/ArrayEqualsCheck.java
@@ -0,0 +1,15 @@
+class A {
+  int[] a, b;
+  String c, d;
+  void fun() {
+    if(a.equals(b)){ }
+    else if (a == b) {}
+    else if (c.equals(d)) {}
+    else if (c.equals(d)) {}
+    else if (method().equals(b)) {}
+  }
+
+  int[] method(){
+    return a;
+  }
+}
